0719d054a9efc3d2f28e4848535a8a183f162352
andelab <bagaricandela@gmail.com>
2021-09-18 18:18:28 +0200

    Added current date and time

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
+function timeNow(timestamp){
+  let date =new Date(timestamp);
+  let hours= date.getHours();
+  if(hours<10){
+    hours=`0 ${hours}`
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10){
+    minutes= ` 0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+  ];
+  let day= days[date.getDay()];
 
+return ` ${day} ${hours}:${minutes}`
+}
 
 
 function displayTemperature(response){
@@ -13,6 +35,8 @@ function displayTemperature(response){
   humidityElement.innerHTML=response.data.main.humidity
   let windElement=document.querySelector("#wind");
   windElement.innerHTML=Math.round(response.data.wind.speed)
+  let dateElement= document.querySelector("#dan");
+  dateElement.innerHTML= timeNow(response.data.dt * 1000)
 }
 let apiKey = "afe9ee1dd9602aa3cd50d8dcf4b72270";
 let city= `Paris`
